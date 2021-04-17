02184b20717d6b70b899ffead52a0632d3a9496f
Petrillooo <gferrer1207@gmail.com>
2021-04-16 23:31:29 -0430

    pagina mango

---
+++ main.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.reloj', { delay: 300 });
+ScrollReveal().reveal('.main_titulo',);
+ScrollReveal().reveal('.main_titulo2',);
+ScrollReveal().reveal('.citas', { delay: 300});
+ScrollReveal().reveal('.main_fotos', { delay: 500});
